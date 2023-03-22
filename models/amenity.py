330bc84a39c0b066e4bb4d1df62652e7f9d0dd61
dshikuta <dshikuta@gmail.com>
2023-03-22 11:52:14 -0700

    dbstorage states and cities

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" State Module for the HBNB project """
+from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """Created Amenity class"""
+    """class for amenities"""
     __tablename__ = 'amenities'
-
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place', secondary='place_amenity',
-                                   viewonly=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary="place_amenity",
+                                       back_populates="amenities")
+    else:
+        name = ""
